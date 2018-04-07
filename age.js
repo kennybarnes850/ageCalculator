dead9a421a4a1f9028622de1d15533ebe824c38d
Kenneth Barnes <kennethbarnes@Kenneths-MacBook-Air.local>
2018-04-07 13:30:15 -0400

    initial commit

---
+++ age.js
@@ -0,0 +1,6 @@
+var age = prompt("What is your age?");
+//I added the .25 because there is a leap year every 4 years//
+var days = age * 365.25;
+
+alert("You are roughly " + days + " days old.");
+
